f56cc8e387224334331f182f93bf222026e4f5dc
unknown <mr.law.jason@gmail.com>
2020-09-29 23:05:34 -0600

    adding components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
-  <h3>I am an App!</h3>
+  <div>
+    <AppHeader/>
+  </div>
 </template>
 
 <script>
-
+import AppHeader  from './components/AppHeader'
 
 export default {
   name: 'App',
- 
+  components: {
+    AppHeader
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
